dc91b4d82ac94554586fa9098de514a9d3e80cd8
Alex Song <alexander.b.song@gmail.com>
2017-02-10 14:02:47 -0500

    OAuth verification added access token now retrieved

---
+++ Vimster/Vimster/LoginViewController.swift
@@ -0,0 +1,60 @@
+//
+//  LoginViewController.swift
+//  Vimster
+//
+//  Created by Alex Song on 2/10/17.
+//  Copyright © 2017 LexCorp. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+import SafariServices
+import OAuthSwift
+
+enum URLHandlerType {
+    case `internal`
+    case external
+    case safari
+}
+
+class LoginViewController: UIViewController {
+    
+    var oauthswift: OAuthSwift?
+    
+    @IBOutlet weak var appTitleLabel: UILabel!
+    @IBOutlet weak var appDescriptionLabel: UILabel!
+    @IBOutlet weak var loginButton: UIButton!
+
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.setupViews()
+    }
+    
+    // MARK: - IBActions
+    @IBAction func loginPressed(_ sender: Any) {
+        self.login()
+    }
+
+    
+    fileprivate func login() {
+        self.oauthLogin()
+    }
+    
+    fileprivate func setupViews() {
+        appTitleLabel.text = VimsterConstants.Login.Strings.appTitle
+        appDescriptionLabel.text = VimsterConstants.Login.Strings.appDescription
+        loginButton.setTitle(VimsterConstants.Login.Strings.buttonTitle, for: .normal)
+    }
+}
+
+extension LoginViewController {
+    
+    fileprivate func oauthLogin() {
+        self.oauthswift = OAuth2.oauthswift
+        OAuth2.oauthswift.authorizeURLHandler = SafariURLHandler(viewController: self, oauthSwift: OAuth2.oauthswift)
+        let _ = OAuth2.handle
+    }
+    
+}
---
+++ Vimster/Vimster/VimsterConstants.swift
@@ -0,0 +1,23 @@
+//
+//  VimsterConstants.swift
+//  Vimster
+//
+//  Created by Alex Song on 2/10/17.
+//  Copyright © 2017 LexCorp. All rights reserved.
+//
+
+import Foundation
+
+struct VimsterConstants {
+
+    struct Login {
+        
+        struct Strings {
+            static let callbackUrl = "vimster://oauth-callback"
+            static let appTitle = "Vimster"
+            static let appDescription = "Make API calls to Vimeo!"
+            static let buttonTitle = "Login"
+        }
+    }
+
+}
